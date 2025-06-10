57eeb675d4fc80e064c48997ce1447ceb843c9de
Vinay Krishna Narne <72037025+vinay682000@users.noreply.github.com>
2025-06-10 03:26:20 +0000

    Update frontend config.js with necessary changes

--- frontend/public/js/config.js
+++ frontend/public/js/config.js
@@ -2,7 +2,7 @@
 // -------------------
 // This one file exports a global `API_BASE` that everyone else can use.
 
-window.API_BASE = "https://psychic-waffle-5gr4jq74vjq7hvgqj-8000.app.github.dev";
+window.API_BASE = "https://turbo-space-computing-machine-j9vq5qw9g672pp66-8000.app.github.dev";
 
 // https://turbo-space-computing-machine-j9vq5qw9g672pp66-8000.app.github.dev
 // https://bug-free-guide-5grxpggpxj5537wrw-8000.app.github.dev
