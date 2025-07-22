a1a447b5fe806522d6d0a93921fc9763195134c8
vinay682000 <narnevinaykrishna@gmail.com>
2025-07-21 21:33:11 -0400

    small change in main.tf

--- iac/main.tf
+++ iac/main.tf
@@ -59,6 +59,52 @@ resource "aws_route_table_association" "public" {
   route_table_id = aws_route_table.public.id
 }
 
+
+resource "aws_network_acl" "public_acl" {
+  vpc_id = aws_vpc.main.id
+  subnet_ids = [aws_subnet.public.id]
+  tags = {
+    Name = "TechInnovators-PublicACL"
+  }
+
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 22
+    to_port    = 22
+  }
+
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 110
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 80
+    to_port    = 80
+  }
+
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 120
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 443
+    to_port    = 443
+  }
+
+  egress {
+    protocol   = "-1"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+  }
+}
+
+
 resource "aws_security_group" "ec2_sg" {
   name        = "techinnovators-ec2-sg"
   description = "Allow HTTP, HTTPS, SSH inbound traffic to EC2"
